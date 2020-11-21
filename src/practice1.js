e925d0d7a0ce6241ef0f652e962750edb6c02bb3
Hui <hui.chen@thoughtworks.com>
2020-11-21 14:39:11 +0800

    add practice

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,18 @@
-const parseData
+
+const parseData = (input) => {
+    const {data, column} = input;
+
+    const parseDate =[];
+
+    for (let itemData of data.values()){
+        let myObject = {};
+        for (let [index,item] of column.entries()){
+            myObject[item.name] = itemData[index]
+        }
+        parseDate.push(myObject)
+    }
+
+    return parseDate
+
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,12 @@
-const inject
+const inject = (items, sections) => {
+
+    console.log(sections)
+
+    for (let  [key,elemSections] of sections.entries()){
+        const {content, index} = elemSections
+        items.splice(index+key, 0, content)
+    }
+    return items
+
+}
 export { inject };
